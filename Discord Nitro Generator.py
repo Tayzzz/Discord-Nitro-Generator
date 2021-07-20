97acfae0cfef788cdf464b26ae60fb70804ea451
Tayzzz <42284450+Tayzzz@users.noreply.github.com>
2021-07-20 15:34:02 +0200

    Update Discord Nitro Generator.py

--- Discord Nitro Generator.py
+++ Discord Nitro Generator.py
@@ -1,110 +1,91 @@
+try:
+    import colorama
+except ModuleNotFoundError as e:
+    modulename = str(e).split("No module named ")[1].replace("'", "")
+    input(f"Please install module with: pip install {modulename}")
+    exit()
+
+from colorama import Fore, init
 from os import system as cmd
 from time import sleep
 import ctypes
 import random
 import string
 import platform
-from colorama import Fore, init
 
 ctypes.windll.kernel32.SetConsoleTitleW(
     "Discord Nitro Generator | Tayzzz | v1.0")
 init(convert=True)
 
+nitros = open("nitros.txt", "a")
+
 
 def main():
 
     clear_console()
-
-    print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Discord Nitro Generator made by {Fore.WHITE}Tayzzz{Fore.LIGHTBLACK_EX} | Licensed under {Fore.WHITE}MIT {Fore.LIGHTBLACK_EX}License | {Fore.WHITE}This tool is for educational purposes only{Fore.RESET}")
-    print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}You can follow me on Github: {Fore.WHITE}https://github.com/Tayzzz{Fore.RESET}")
-
+    print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Discord Nitro Generator made by {Fore.WHITE}Tayzzz{Fore.LIGHTBLACK_EX} | Licensed under {Fore.WHITE}MIT {Fore.LIGHTBLACK_EX}License | {Fore.WHITE}This tool is for educational purposes only{Fore.RESET}\n{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}You can follow me on Github: {Fore.WHITE}https://github.com/Tayzzz{Fore.RESET}")
     try:
-        amount = int(input(
-            f"\n{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}How much codes will be generated: {Fore.WHITE}"))
+        try:
+            nitroAmount = int(input(
+                f"\n{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}How much codes will be generated: {Fore.WHITE}"))
+        except ValueError:
+            print(
+                f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Amount must be {Fore.WHITE}a number")
+            sleep(2)
+            return main()
 
-        nitro = input(
-            f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}What kind of nitro do you want ? {Fore.WHITE}classic {Fore.LIGHTBLACK_EX}or {Fore.WHITE}boost {Fore.LIGHTBLACK_EX}? (Classic Nitro is 16chars and Boost Nitro is 24chars): {Fore.WHITE}")
+        nitroType = input(f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}What kind of nitro do you want ? {Fore.WHITE}classic {Fore.LIGHTBLACK_EX}or {Fore.WHITE}boost {Fore.LIGHTBLACK_EX}? (Classic Nitro is 16chars and Boost Nitro is 24chars): {Fore.WHITE}")
 
-        link = input(
+        if nitroType == "boost" or nitroType == "classic":
+            pass
+        else:
+            print(
+                f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}boost {Fore.LIGHTBLACK_EX}or {Fore.WHITE}classic{Fore.RESET}")
+            sleep(2)
+            return main()
+
+        nitroLink = input(
             f"{Fore.WHITE}[ {Fore.YELLOW}> {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Do you want {Fore.WHITE}https://discord.com/gifts/ {Fore.LIGHTBLACK_EX}behind the code ? ({Fore.WHITE}yes {Fore.LIGHTBLACK_EX}or {Fore.WHITE}no): {Fore.WHITE}")
-        if "boost" in nitro or "classic" in nitro:
-            pass
-        else:
-            print(
-                f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}boost {Fore.LIGHTBLACK_EX}or {Fore.WHITE}classic")
-            sleep(3)
-            exit()
 
-        if "yes" in link or "no" in link:
+        if nitroLink == "yes" or nitroLink == "no":
             pass
         else:
             print(
-                f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}yes {Fore.LIGHTBLACK_EX}or {Fore.WHITE}no")
-            sleep(3)
-            exit()
-    except ValueError:
-        print(
-            f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Amount must be {Fore.WHITE}a number")
-        sleep(3)
-        clear_console()
-        exit()
+                f"{Fore.WHITE}[ {Fore.RED}! {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Answer must be {Fore.WHITE}yes {Fore.LIGHTBLACK_EX}or {Fore.WHITE}no{Fore.RESET}")
+            sleep(2)
+            return main()
     except KeyboardInterrupt:
         clear_console()
         exit()
 
-    a = 0
-    nitros = open("nitro.txt", "w")
-    nitros.write("")
+    gen(nitroAmount, nitroType, nitroLink)
 
-    print()
 
-    def gen():
+def gen(nitroAmount, nitroType, nitroLink):
+
+    amount = 0
+
+    with open("nitros.txt", "w") as file:
+        file.write("")
+
+    for _ in range(nitroAmount):
+        genCode = "".join(random.choice(string.digits+string.ascii_letters)
+                          for _ in range(24)) if nitroType == "boost" else "".join(random.choice(string.digits+string.ascii_letters)
+                                                                                   for _ in range(16))
+        nitro = f"https://discord.com/gifts/{genCode}" if nitroLink == "yes" else genCode
+
         try:
-            ctypes.windll.kernel32.SetConsoleTitleW(
-                f"Discord Nitro Generator | {a-1} nitro(s) generated")
-
-            if "classic" in nitro:
-                code = "".join(random.choice(string.digits+string.ascii_letters)
-                               for _ in range(16))
-            else:
-                code = "".join(random.choice(string.digits+string.ascii_letters)
-                               for _ in range(24))
-            if link == "yes":
-                msg = f"https://discord.com/gifts/{code}"
-            else:
-                msg = code
-
             print(
-                f"{Fore.WHITE}[ {Fore.YELLOW}- {Fore.WHITE}] {msg}")
-
-            nitros.write(f"{msg}\n")
-
-        except KeyboardInterrupt:
-            nitros.close()
-            input(
-                f"\n{Fore.WHITE}[ {Fore.GREEN}> {Fore.WHITE}] {len(open('nitro.txt').readlines())} nitros have been generated. (Press any key to close the generator)")
-            exit()
-
-    if amount == 0:
-        while True:
-            a += 1
-            gen()
-
-    else:
-
-        while a != amount:
-            gen()
-            a += 1
+                f"{Fore.WHITE}[ {Fore.YELLOW}- {Fore.WHITE}] {Fore.RESET}{nitro}")
+            nitros.write(
+                f"{nitro}\n")
+            amount += 1
+        except:
+            break
 
     nitros.close()
-    for line in open('nitro.txt').readlines():
-        line.strip("\n")
-    ctypes.windll.kernel32.SetConsoleTitleW(
-        f"Discord Nitro Generator | Done !")
-
     input(
-        f"\n{Fore.WHITE}[ {Fore.GREEN}> {Fore.WHITE}] {a} nitros have been generated. (Press any key to close the generator)")
-    exit()
+        f"\n{Fore.WHITE}[ {Fore.GREEN}> {Fore.WHITE}] {amount} nitros have been generated. (Press any key to close the generator)")
 
 
 def clear_console():
